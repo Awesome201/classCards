76992012b9c4667304aad130a540b8d92126cd3a
zachary schumpert <buphnezz@gmail.com>
2018-01-29 15:08:22 -0800

    ....i was saving to the wrong index.html, needed to move everything to the about.html doh!

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// userName variable
+var userName;
+
+//get element information
+var inputElUserName = document.getElementById('userName');
+var formElLoginForm = document.getElementById('loginForm');
+
+//if no local, create it. else, use it
+function checkLocalStorage() {
+  if (!localStorage.userName) {
+    localStorage.userName = JSON.stringify();
+  } else {
+    userName = JSON.parse(localStorage.userName);
+  }
+}
+
+//set the value to user input and save to local storage
+function handleLogin (e) {
+  e.preventDefault();
+  userName = inputElUserName.value;
+  localStorage.userName = JSON.stringify(userName);
+  window.location = 'profile/profile.html';
+}
+
+//call login function on submit
+formElLoginForm.addEventListener('submit', handleLogin);
+
+//call local check
+checkLocalStorage();
