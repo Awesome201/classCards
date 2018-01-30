6fbecf060493ee07ddfc37c4c28c55ec7085c249
zachary schumpert <buphnezz@gmail.com>
2018-01-30 14:55:54 -0800

    creating a separate function that will render ONLY our 5 AboutUs cards

--- about/js/about.js
+++ about/js/about.js
@@ -7,6 +7,7 @@ var cardElement = document.getElementById('all-cards');
 Profile.allProfiles = [];
 
 
+
 // this holds all the properties for each Profile that will be generated. 
 function Profile(userName, userAvatar, userHobby, userColor, knownLanguage, interestedLanguage) {
   this.userName = userName;
@@ -18,8 +19,16 @@ function Profile(userName, userAvatar, userHobby, userColor, knownLanguage, inte
   Profile.allProfiles.push(this);
 }
 
-function createAboutUsCards() {
+
+function createAllCards() {
   for(var i in Profile.allProfiles) {
+    Profile.allProfiles[i].render();
+  }
+}
+
+function createOnlyOurAboutUsCards() {
+  var i = 0;
+  for (var i = 0; onlyOurAboutUsCards > Profile.allProfiles[4]; onlyOurAboutUsCards++) {
     Profile.allProfiles[i].render();
   }
 }
@@ -69,56 +78,7 @@ Profile.prototype.render = function() {
   var aboutUsDescriptionEl = document.createElement('p');
   aboutUsDescriptionEl.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi accumsan diam imperdiet diam laoreet, in dictum sapien blandit.';
   divHoldTheCards.appendChild(aboutUsDescriptionEl);
-  
 }
-
-
-
-
-
-// this is a function that when called, will append the appropriate element to the section of the DOM and assign the appropriate userName, img, hobby, knownLanguage and interestedLanguage value to the corresponding element that was just created.
-
-// function createCard(profileObj) {
-//   for (var createCard in Profile.allProfiles.length) {
-
-//     // Set the section ID to the section
-//     sectionElFlipContainer.setAttribute('id', profileObj.userName + 'li');
-//     sectionElFlipContainer.setAttribute('class', 'flipContainer');
-//     var sectionElFlipper = document.createElement('li');
-//     sectionElFlipper.setAttribute('class', 'flipper');
-//     var sectionElFront = document.createElement('li');
-//     sectionElFront.setAttribute('class', 'front');
-//     var sectionElBack = document.createElement('li');
-//     sectionElBack.setAttribute('class', 'back');
-
-//     // Create the h2 element
-//     var h2El = document.createElement('h2');
-
-//     // Set its contents to the userName
-//     h2El.textContent = profileObj.userName;
-
-//     // appened the h2 to the section
-//     sectionElFront.appendChild(h2El);
-
-//     // Create the img elemnt for the avatar
-//     var imgEl = document.createElement('img');
-//     imgEl.src = profileObj.userAvatar;
-//     imgEl.alt = profileObj.userName;
-//     sectionElFront.appendChild(imgEl);
-//     sectionElFlipper.appendChild(sectionElFront);
-
-//     // add interests
-//     // Create the ul to house the interests
-//     var ulEl = document.createElement('ul');
-//     ulEl.appendChild(buildLiEl('Hobby: ', profileObj.userHobby));
-//     ulEl.appendChild(buildLiEl('Color: ', profileObj.userColor));
-//     ulEl.appendChild(buildLiEl('Code Known : ', profileObj.knownLanguage));
-//     ulEl.appendChild(buildLiEl('Code Interest : ', profileObj.interestedLanguage));
-//     sectionElBack.appendChild(ulEl);
-//     sectionElFlipper.appendChild(sectionElBack);
-//     sectionElFlipContainer.appendChild(sectionElFlipper);
-//     return sectionElFlipContainer;
-//   }
   
   // create new instances of the Profile object
   new Profile('Zachary', '../img/wizzard.jpg', 'watching anime', '#bada55', 'Java', 'JavaScript');
@@ -127,9 +87,8 @@ Profile.prototype.render = function() {
   new Profile('Ramon', '../img/monk.png', 'racing motorsports', '#ff0000', 'JavaScript', 'Python');
   new Profile('Kevin', '../img/fighter.png', 'martial arts', '#008000', 'cSharp', 'JavaScript');
   
-  
   // have those new instances display in the about.html
   
-  createAboutUsCards();
-  
+  createOnlyOurAboutUsCards();
+
   //how do I make the new Profile object ex: first-card, second-card etc. appear on html?  We used table for salmon so do I make each instance of a card a table?
